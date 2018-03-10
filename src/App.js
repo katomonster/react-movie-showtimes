4993ffba7706b3753e75431202625a335afc1e9b
katomonster <junekato@gmail.com>
2018-03-10 08:28:02 -0800

    refactor sorting showtime

--- src/App.js
+++ src/App.js
@@ -135,20 +135,19 @@ const MovieList = (props) => {
 
     return (
         props.movieData.map((data, i) => {
-            let sorted = [];
-            const obj = data.showtimes.map((time) => {
+            let sorted = data.showtimes.map((time) => {
                 const split = time.split(" ");
                 const amPm = split[1];
-                const t = split[0].split(":");
-                const hour = amPm === "am" ? parseInt(t[0]) * 60 : (parseInt(t[0]) + 12) * 60;
-                const min = parseInt(t[1]);
+                const hm = split[0].split(":");
+                const hour = amPm === "am" ? parseInt(hm[0], 10) * 60 : (parseInt(hm[0], 10) + 12) * 60;
+                const min = parseInt(hm[1], 10);
                 const startTime = hour + min;
-                sorted.push({time: time, startTime: startTime});
+                return {formatted: time, number: startTime};
             });
-            sorted = sorted.sort((a, b) => a.startTime - b.startTime);
+            sorted = sorted.sort((a, b) => a.number - b.number);
             const showtimes = sorted.map((time, i) => {
                 return (
-                    <span key={i} className = {time.startTime < curtime ? "time--past" : ""}>{time.time}</span>
+                    <span key={i} className = {time.number < curtime ? "time--past" : ""}>{time.formatted}</span>
                 );
             });
             return (
